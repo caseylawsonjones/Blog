20591995ec27f851c44df502ff6518fb5fd54b01
Casey L. Jones <caseylawsonjones@yahoo.com>
2017-09-15 16:52:37 -0400

    Initial commit

---
+++ Blog/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models {
+    public class Comment {
+
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public string MediaUrl { get; set; }
+        public string UpdateReason { get; set; }
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models {
+    public class Post {
+    
+        public Post() {
+            Comments = new HashSet<Comment>();
+        }
+        public int Id { get; set;}
+        public string AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public string MediaUrl { get; set; }
+        public bool Published { get; set; }
+        public string Slug { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+        //public virtual ICollection<Reaction> Reactions { get; set; }
+
+
+   }
+
+
+
+
+
+
+}
